b967a419ba619e5b14e8f8b02ced77ecc107333d
Max Brown <maxbrown0101@gmail.com>
2016-03-14 14:33:07 +1300

    added ecl project

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * everything in here til structure emerges tbh
+ * @author brownmax1
+ *
+ */
+public class Main {
+
+
+	public static void main(String[] args){
+		System.out.println("hi");
+	}
+}
