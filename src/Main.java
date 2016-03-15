d1556e6e32da25d9706bf0b0f923864948c72d19
Max Brown <maxbrown0101@gmail.com>
2016-03-16 11:55:24 +1300

    added relative paths and removed some comments and shit

--- src/Main.java
+++ src/Main.java
@@ -24,8 +24,16 @@ public class Main {public Main() {
 }
 	//TODO: we will probably want to track the og 4 values as we go through because just outputting a map that is n->class for the test data is not going to let us see what irises ended up where. maybe n -> irisResult{vector, class}
 
-	public static final String PATH_TO_TRAINING = "C:\\school_work\\2016tri1\\AI\\assign1\\part1NearestNeighbour\\NearestNeighbourGit\\data\\iris-training.txt";
-	public static final String PATH_TO_TEST = "C:\\school_work\\2016tri1\\AI\\assign1\\part1NearestNeighbour\\NearestNeighbourGit\\data\\iris-test.txt";
+	//my machine hard coded paths
+	//public static final String PATH_TO_TRAINING = "C:\\school_work\\2016tri1\\AI\\assign1\\part1NearestNeighbour\\NearestNeighbourGit\\data\\iris-training.txt";
+	//public static final String PATH_TO_TEST = "C:\\school_work\\2016tri1\\AI\\assign1\\part1NearestNeighbour\\NearestNeighbourGit\\data\\iris-test.txt";
+
+	public static final String PATH_TO_TRAINING = "data/iris-test.txt";
+	public static final String PATH_TO_TEST = "data/iris-training.txt";
+
+
+
+
 	public static final int k = 3;//TODO: the code down the bottom that deals with talleys only works with like a talley size of 3. also the logic is all hardcoded to be 3...
 
 	public static void main(String[] args) throws IOException{
@@ -40,43 +48,17 @@ public class Main {public Main() {
 		double[] ranges = {3.6, 2.4, 5.9, 2.4};
 		//map the test irises to a class using the training irises
 		HashMap<String[], String> results = classify(k, ranges,  trainingIrises, testIrises);
-		
-		
-		
-		
-		/*//create our training data
-		ArrayList<String[]> trainingIrises = loadIrisDataFromFile(PATH_TO_TRAINING);
-		HashMap<Float, String> training = generateWeightToClassMap(trainingIrises);
-		for(Float eachWeight: training.keySet()){
-			System.out.println(eachWeight);
-			System.out.println(training.get(eachWeight));
-		}
-		
-		//create our test data
-		ArrayList<String[]> testingIrises = loadIrisDataFromFile(PATH_TO_TEST);
-		HashMap<String[], String> testResult = generateTestResults(3, testingIrises, training);*/
-		
-		/*//print out our test result
-		for(String[] eachIris: testResult.keySet()){
-			System.out.println("this is the actual information for an iris: ");
-			System.out.println(eachIris[0]);
-			System.out.println(eachIris[1]);
-			System.out.println(eachIris[2]);
-			System.out.println(eachIris[3]);
-			System.out.println(eachIris[4]);
-			System.out.println("and this is the class the ai came up with: ");
-			System.out.println(testResult.get(eachIris));
-			System.out.println("\n ======================== \n");
-		}*/
+
+
 	}
 
 
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
 
 
 
@@ -88,8 +70,8 @@ public class Main {public Main() {
 	private static HashMap<String[], String> classify(int k, double[] ranges, ArrayList<String[]> trainingIrises,
 			ArrayList<String[]> testIrises) {
 		HashMap<String[], String> results = new HashMap<>();
-		
-		
+
+
 		for(String[] eachTestIris: testIrises){
 			ConcurrentHashMap<Double, String[]> neighbours = new ConcurrentHashMap<>();
 			//populate the closest neighbours with filler values to start out
@@ -100,7 +82,7 @@ public class Main {public Main() {
 			for(int i = 0; i < k; i++){
 				neighbours.put(findDistance(ranges, eachTestIris, trainingIrises.get(i)), trainingIrises.get(i));
 			}
-			assert(neighbours.size() == k):"there should only be k neighbours:" + neighbours.size();
+			assert(neighbours.size() == k):"there should be k neighbours:" + neighbours.size();
 			//now go through all of the irises in the training set and store the closest ones to this test iris as neighbours NOTE: if two training irises are equal distance from our test iris, the first one encountered is saved. This should probably be randomised because it favours the class that appears in the file first.
 			for(String[] eachTrainingIris: trainingIrises){
 				assert(neighbours.size() == k):"there should only be k neighbours:" + neighbours.size();
@@ -117,7 +99,7 @@ public class Main {public Main() {
 						//assert(!neighbours.keySet().contains(distance)):"noooo looks like we are trying to put something in the map that is already there :))):" + distance;
 						neighbours.put(distance, eachTrainingIris);
 						//ONLY remove the one we just iterated over in the case that when we inserted the new key, we didn't just overwrite an older entry
-						//NOTE: THIS IS INCORRECT. e.g. if you had 20, 15, 25 in the neighbours and you encounter 20. It will just replace 20 instead of 25, so you don't have optimal configuration. TODO: MOST SRS TODO: you should not be using a map. It is allowed to have repeats. You should also not re-iterate over the first K training irises because then e.g. if the first iris in the training list was really close, we might get it once in the dummy arbitrary fill up period, and then again in the actual traversal period. wew. 
+						//NOTE: THIS IS INCORRECT. e.g. if you had 20, 15, 25 in the neighbours and you encounter 20. It will just replace 20 instead of 25, so you don't have optimal configuration. TODO: MOST SRS TODO: you should not be using a map. It is allowed to have repeats. You should also not re-iterate over the first K training irises because then e.g. if the first iris in the training list was really close, we might get it once in the dummy arbitrary fill up period, and then again in the actual traversal period. wew.
 						if(neighbours.size() == k + 1){
 							iter.remove();
 						}
@@ -126,7 +108,8 @@ public class Main {public Main() {
 					}
 				}
 			}
-			System.out.println(" \n \n \n these that neighbours for a " + eachTestIris[4] + ": ");
+			System.out.println(" \n \n \n these are the neighbours neighbours for a " + eachTestIris[4] + ": ");
+			System.out.println(eachTestIris[0] + " " + eachTestIris[1] + " " + eachTestIris[2] + " " + eachTestIris[3]);
 			for(double eachNeighbour: neighbours.keySet()){
 				System.out.println(eachNeighbour);
 				System.out.println(neighbours.get(eachNeighbour)[4]);
@@ -134,7 +117,7 @@ public class Main {public Main() {
 			System.out.println("\n \n \n");
 			//now we have all of the neighbours of this node, find the majority class
 			//NOTE: there should be enums e.g. 1 -> Iris Setosa etc so that we can just do the tallies and then easily tell which is the majority
-			ArrayList<Integer> tally = new ArrayList<>(Arrays.asList(0, 0, 0)); 
+			ArrayList<Integer> tally = new ArrayList<>(Arrays.asList(0, 0, 0));
 			for(double eachNeighbourDist: neighbours.keySet()){
 				assert(tally.size() == k);
 				if(neighbours.get(eachNeighbourDist)[4].equals("Iris-setosa")){
@@ -159,14 +142,14 @@ public class Main {public Main() {
 				assert(false): "has to be one of those";
 			}
 		}
-		
+
 		return results;
 	}
 
 
-	
-	
-	
+
+
+
 	/**
 	 * finds kinda normalised euclidean distance between two irises
 	 * @param ranges the absolute difference between the smallest and largest measurement in each of the 4 categories
@@ -184,22 +167,18 @@ public class Main {public Main() {
 			sum += normalised;
 		}
 		double result = Math.sqrt(sum);
-		
+
 		return result;
 	}
 
 
 
 
-
-	
-
-	
 	/*	   1. sepal length in cm
 	   2. sepal width in cm
 	   3. petal length in cm
 	   4. petal width in cm
-	   5. class: 
+	   5. class:
 	      -- Iris Setosa
 	      -- Iris Versicolour
 	      -- Iris Virginica*/
