ce1a3c4ffa64066aba8beaf8993f6a4f9c9e0397
Max <maxbrown0101@gmail.com>
2016-03-15 23:12:10 +1300

    got the algorithm working. There are a few possible bugs/limitations described in TODOs. At the moment only works with a K value of 3. It is surprisingly effective. The setosa's are all detected perfectly. There is a very minor amount of conflation between the other two sets

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,12 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import com.opencsv.CSVReader;
 
@@ -14,17 +19,32 @@ import com.opencsv.CSVReader;
  * @author brownmax1
  *
  */
-public class Main {
-
+public class Main {public Main() {
+	// TODO Auto-generated constructor stub
+}
 	//TODO: we will probably want to track the og 4 values as we go through because just outputting a map that is n->class for the test data is not going to let us see what irises ended up where. maybe n -> irisResult{vector, class}
 
 	public static final String PATH_TO_TRAINING = "C:\\school_work\\2016tri1\\AI\\assign1\\part1NearestNeighbour\\NearestNeighbourGit\\data\\iris-training.txt";
 	public static final String PATH_TO_TEST = "C:\\school_work\\2016tri1\\AI\\assign1\\part1NearestNeighbour\\NearestNeighbourGit\\data\\iris-test.txt";
-
+	public static final int k = 3;//TODO: the code down the bottom that deals with talleys only works with like a talley size of 3. also the logic is all hardcoded to be 3...
 
 	public static void main(String[] args) throws IOException{
+		System.out.println("starting...");///THREE EXTRA 0S IN CONSOLE BUFFER
+		//load in the training data from the file
+		ArrayList<String[]> trainingIrises = loadIrisDataFromFile(PATH_TO_TRAINING);
+		//load in the test data from the file
+		ArrayList<String[]> testIrises = loadIrisDataFromFile(PATH_TO_TEST);
+		//find the ranges of the four measurements
+		//double[] ranges = findRanges(trainingIrises, testIrises);
+		//TODO: maybe this shouldnnt be hardcoded
+		double[] ranges = {3.6, 2.4, 5.9, 2.4};
+		//map the test irises to a class using the training irises
+		HashMap<String[], String> results = classify(k, ranges,  trainingIrises, testIrises);
 		
-		//create our training data
+		
+		
+		
+		/*//create our training data
 		ArrayList<String[]> trainingIrises = loadIrisDataFromFile(PATH_TO_TRAINING);
 		HashMap<Float, String> training = generateWeightToClassMap(trainingIrises);
 		for(Float eachWeight: training.keySet()){
@@ -34,9 +54,9 @@ public class Main {
 		
 		//create our test data
 		ArrayList<String[]> testingIrises = loadIrisDataFromFile(PATH_TO_TEST);
-		HashMap<String[], String> testResult = generateTestResults(5, testingIrises, training);
+		HashMap<String[], String> testResult = generateTestResults(3, testingIrises, training);*/
 		
-		//print out our test result
+		/*//print out our test result
 		for(String[] eachIris: testResult.keySet()){
 			System.out.println("this is the actual information for an iris: ");
 			System.out.println(eachIris[0]);
@@ -47,95 +67,127 @@ public class Main {
 			System.out.println("and this is the class the ai came up with: ");
 			System.out.println(testResult.get(eachIris));
 			System.out.println("\n ======================== \n");
-		}
+		}*/
 	}
 
 
-	/**
-	 * uses training data to map irises to the class they seems to belong to
-	 * @param irises the irises we are mapping using our training data
-	 * @param training the map we produced from our training that is weight->class
-	 * @param k the amount of neighbours to compare each iris to
-	 * @return iris -> class map (NOTE THAT THE iris COMPONENT IN THIS CASE INCLUDES THE /ACTUAL/ CLASS OF THE IRIS. THIS WILL ALLOW EASY INTUITIVE CHECKING OF HOW WELL MY CLASSIFIER DOES)
-	 */
-	//TODO: the part that looks for closest neighbour ONLY replaces the current neighbour if we find one less than. Not less than or equal to. So it favours the first one it encounters.
-	private static HashMap<String[], String> generateTestResults(int k, ArrayList<String[]> irises, HashMap<Float, String> training){
-		assert(k < training.keySet().size()):"k must be less than the amount of irises in the training map";
-			
-		HashMap<String[], String> irisToClass = new HashMap<>();
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+
+
+
+	private static HashMap<String[], String> classify(int k, double[] ranges, ArrayList<String[]> trainingIrises,
+			ArrayList<String[]> testIrises) {
+		HashMap<String[], String> results = new HashMap<>();
 		
-		for(String[] eachIris: irises){
-			//find the weight for this test iris
-			float[] irisMeasurements = {Float.valueOf(eachIris[0]), Float.valueOf(eachIris[1]), Float.valueOf(eachIris[2]), Float.valueOf(eachIris[3])};
-			float irisWeight = generateWeight(irisMeasurements);
-			//now we have the weight of this iris, try to find which of the training weights it is closest to
-			float[] neighbours = new float[k];
-			//populate the closest neighbours with filler values to start out with (just get the first values from the training map)
+		
+		for(String[] eachTestIris: testIrises){
+			ConcurrentHashMap<Double, String[]> neighbours = new ConcurrentHashMap<>();
+			//populate the closest neighbours with filler values to start out
+			//WE REMOVE THESE NEIGHBOURS FROM THE trainingIrises list as we go so that
+			//we do not encounter them when we are traversing all of the training irises later one
+			//(when we do that, we get the problem of generating exactly the same key/distance as we have generated before
+			//which "overwrites an entry in the map. We were still taking one out, but
 			for(int i = 0; i < k; i++){
-				neighbours[i] = (float) training.keySet().toArray()[i]; //TODO: should optimise it a lil by only converting to array once
+				neighbours.put(findDistance(ranges, eachTestIris, trainingIrises.get(i)), trainingIrises.get(i));
 			}
-			//now store the k closest weights in the neighbours array NOTE: they are not stored in any particular order. (actually they might get put in order because I'm always checking to replace the first index first, so it only won't go there if the first index is already bigger but others aren't.)
-			for(float eachTrainingWeight: training.keySet()){
-				for(int i = 0; i < k; i++){
-					if(Math.abs(eachTrainingWeight - irisWeight) < neighbours[i]){
-						//we found a closer neighbour, so add that neighbour to the neighbour array and stop comparing to other neighbours
-						neighbours[i] = eachTrainingWeight;
+			assert(neighbours.size() == k):"there should only be k neighbours:" + neighbours.size();
+			//now go through all of the irises in the training set and store the closest ones to this test iris as neighbours NOTE: if two training irises are equal distance from our test iris, the first one encountered is saved. This should probably be randomised because it favours the class that appears in the file first.
+			for(String[] eachTrainingIris: trainingIrises){
+				assert(neighbours.size() == k):"there should only be k neighbours:" + neighbours.size();
+				//calculate the distance from the training iris to the test iris
+				double distance = findDistance(ranges, eachTestIris, eachTrainingIris);
+				//check if the distance is less than the distance to any of the current neighbours
+				Iterator<Map.Entry<Double, String[]>> iter = neighbours.entrySet().iterator();
+				while(iter.hasNext()){
+					Map.Entry<Double, String[]> eachNeighbour = iter.next();
+					if(distance < eachNeighbour.getKey()){
+						//we found a closer neighbour than one that is currently in the neighbours
+						//we need to take the old one out, put the new one in, and then stop comparing to neighbours
+						assert(neighbours.size() == k):"there should only be k neighbours:" + neighbours.size();
+						//assert(!neighbours.keySet().contains(distance)):"noooo looks like we are trying to put something in the map that is already there :))):" + distance;
+						neighbours.put(distance, eachTrainingIris);
+						//ONLY remove the one we just iterated over in the case that when we inserted the new key, we didn't just overwrite an older entry
+						//NOTE: THIS IS INCORRECT. e.g. if you had 20, 15, 25 in the neighbours and you encounter 20. It will just replace 20 instead of 25, so you don't have optimal configuration. TODO: MOST SRS TODO: you should not be using a map. It is allowed to have repeats. You should also not re-iterate over the first K training irises because then e.g. if the first iris in the training list was really close, we might get it once in the dummy arbitrary fill up period, and then again in the actual traversal period. wew. 
+						if(neighbours.size() == k + 1){
+							iter.remove();
+						}
+						assert(neighbours.size() == k):"there should be k neighbours:" + neighbours.size();
 						break;
 					}
 				}
 			}
-			//just a test that neighbours are validish
-			for(int i = 0; i < k; i++){
-				assert(training.keySet().contains(neighbours[i])):"the stored neighbours values must be keys in the training map";
+			System.out.println(" \n \n \n these that neighbours for a " + eachTestIris[4] + ": ");
+			for(double eachNeighbour: neighbours.keySet()){
+				System.out.println(eachNeighbour);
+				System.out.println(neighbours.get(eachNeighbour)[4]);
 			}
-			//whichever class appears the most times in the neighbours is the class we will map this iris to
-			//TODO: maybe make this class tally a map of className->tally. THIS IS REALLY BAD DESIGN NOT EASILY EXTENSIBLE FOR MORE CLASSES OF IRIS ETC
-			int[] classTally = new int[3];//0 is Iris-setosa, 1 is Iris-versicolor, 2 is Iris-virginica
-			for(int i = 0; i < k; i++){
-				if(training.get(neighbours[i]).equals("Iris-setosa")){
-					classTally[0]++;
-				}else if(training.get(neighbours[i]).equals("Iris-versicolor")){
-					classTally[1]++;
+			System.out.println("\n \n \n");
+			//now we have all of the neighbours of this node, find the majority class
+			//NOTE: there should be enums e.g. 1 -> Iris Setosa etc so that we can just do the tallies and then easily tell which is the majority
+			ArrayList<Integer> tally = new ArrayList<>(Arrays.asList(0, 0, 0)); 
+			for(double eachNeighbourDist: neighbours.keySet()){
+				assert(tally.size() == k);
+				if(neighbours.get(eachNeighbourDist)[4].equals("Iris-setosa")){
+					tally.set(0, tally.get(0) + 1);
+				}else if(neighbours.get(eachNeighbourDist)[4].equals("Iris-versicolor")){
+					tally.set(1, tally.get(1) + 1);
+				}else if(neighbours.get(eachNeighbourDist)[4].equals("Iris-virginica")){
+					tally.set(2, tally.get(2) + 1);
 				}else{
-					assert(training.get(neighbours[i]).equals("Iris-virginica"));
-					classTally[2]++;
+					assert(false): "has to be one of those three" + neighbours.get(eachNeighbourDist)[4];
 				}
 			}
-			//finally, map this iris to the class that it seems to belong to
-			if(classTally[0] > classTally[1] && classTally[0] > classTally[2]){
-				irisToClass.put(eachIris, "Iris-setosa");
-			}else if(classTally[1] > classTally[0] && classTally[1] > classTally[2]){
-				irisToClass.put(eachIris, "Iris-versicolor");
-			}else{//TODO: when there is a three way tie, we will be putting the iris into this class. it should be random.
-				irisToClass.put(eachIris, "Iris-virginica");
+			//so we tallied the neighbours, map this test iris to the class that has the highest tally
+			//NOTE: need to take care of the case at the top where they are all equal, in which case a random class should be chosen or something. Atm setosa gets advantage.
+			if(tally.get(0) >= tally.get(1) && tally.get(0) >= tally.get(2)){
+				results.put(eachTestIris, "Iris-setosa");
+			}else if(tally.get(1) >= tally.get(0) && tally.get(1) >= tally.get(2)){
+				results.put(eachTestIris, "Iris-versicolor");
+			}else if(tally.get(2) >= tally.get(0) && tally.get(2) >= tally.get(1)){
+				results.put(eachTestIris, "Iris-virginica");
+			}else{
+				assert(false): "has to be one of those";
 			}
-			
 		}
 		
-		
-		return irisToClass;
+		return results;
 	}
-	
-	
-	
+
+
 	
 	
 	
 	/**
-	 * takes a list of iris data and creates a trained map thing weight -> class
-	 * @throws IOException
+	 * finds kinda normalised euclidean distance between two irises
+	 * @param ranges the absolute difference between the smallest and largest measurement in each of the 4 categories
+	 * @param eachTestIris an iris to compare (5 value vector but we only look at first four)
+	 * @param eachTrainingIris an iris to compare
+	 * @returnthe normalised difference
 	 */
-	//TODO: should consider using a different data structure. We are going to be looking for keys that are SIMILAR to the n values we create in the test stage. Not exactly the same.
-	public static HashMap<Float, String> generateWeightToClassMap(ArrayList<String[]> irises) throws IOException{
-		
-		HashMap<Float, String> trainingMap = new HashMap<>();
-		for(String[] eachIris: irises){
-			float[] irisMeasurements = {Float.valueOf(eachIris[0]), Float.valueOf(eachIris[1]), Float.valueOf(eachIris[2]), Float.valueOf(eachIris[3])};
-			float weight = generateWeight(irisMeasurements);
-			trainingMap.put(weight, eachIris[4]);//TODO: should really try putting something in here other than just the class so that I can identify which source data got assigned which weight etc
+	private static double findDistance(double[] ranges, String[] eachTestIris, String[] eachTrainingIris) {
+		double sum = 0.0;
+		for(int eachAttributeIndex = 0; eachAttributeIndex < ranges.length; eachAttributeIndex ++){
+			double difference = Double.valueOf(eachTestIris[eachAttributeIndex]) - Double.valueOf(eachTrainingIris[eachAttributeIndex]);
+			double differenceSquared = difference * difference;
+			double range = ranges[eachAttributeIndex];
+			double normalised = differenceSquared / (range * range);
+			sum += normalised;
 		}
-		return trainingMap;
+		double result = Math.sqrt(sum);
+		
+		return result;
 	}
+
 
 
 
@@ -151,15 +203,6 @@ public class Main {
 	      -- Iris Setosa
 	      -- Iris Versicolour
 	      -- Iris Virginica*/
-
-	/**
-	 * takes an array of the iris data (4 decimal values) and generates the weight value which is what will be used to map to classes
-	 * 
-	 */
-	public static float generateWeight(float[] irisMeasurements){
-		assert(irisMeasurements.length == 4):"I can't generate a weight with an array the wrong size";
-		return irisMeasurements[0] + irisMeasurements[1] + irisMeasurements[2] + irisMeasurements[3];
-	}
 
 
 	/**
